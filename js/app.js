087c4238138abafa6677c3d949371e07de845103
Rodwyn <rodwynmoreno@gmail.com>
2021-06-04 18:42:57 -0500

    Implemented put BD method.

--- js/app.js
+++ js/app.js
@@ -8,11 +8,19 @@
 
   // EDITING STARTS HERE (you dont need to edit anything above this line)
 
-  var db = false;
+  var db = new PouchDB('todos');
   var remoteCouch = false;
 
   // We have to create a new todo document and enter it in the database
   function addTodo(text) {
+    var todo = {
+      _id: new Date().toISOString(),
+      title: text,
+      completed: false
+    };
+    db.put( todo )
+      .then(console.log('insertado'))
+      .catch(console.log());
   }
 
   // Show the current list of todos by reading them from the database
